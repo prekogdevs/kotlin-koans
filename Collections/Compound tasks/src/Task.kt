bbafd61013317da651f3d1d989513297c7108d40
Gergely Szebenyi <gergely_szebenyi@epam.com>
2021-03-03 17:37:10 +0100

    Solution for Compound tasks

--- Collections/Compound tasks/src/Task.kt
+++ Collections/Compound tasks/src/Task.kt
@@ -1,14 +1,19 @@
 // Find the most expensive product among all the delivered products
 // ordered by the customer. Use `Order.isDelivered` flag.
-fun findMostExpensiveProductBy(customer: Customer): Product? {
-    TODO()
-}
+fun findMostExpensiveProductBy(customer: Customer) =
+        customer.orders
+                .filter { it.isDelivered }
+                .flatMap { it.products }
+                .maxBy { it.price }
+
 
 // Count the amount of times a product was ordered.
 // Note that a customer may order the same product several times.
-fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
-    TODO()
-}
+fun Shop.getNumberOfTimesProductWasOrdered(product: Product) =
+        this.customers
+                .flatMap { it.getOrderedProducts() }
+                .count { it == product }
+
 
 fun Customer.getOrderedProducts(): List<Product> =
-        /* TODO */
+        this.orders.flatMap { it.products }.toList()
